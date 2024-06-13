0ccf7b6963b8b4de28b1d812439e29717cc8d72b
Immanuel Ehsan <foxeian26@gmail.com>
2024-06-13 11:20:34 +0300

    Navbar links & types

---
+++ src/data/navigation.ts
@@ -0,0 +1,9 @@
+import type { TNavigation } from "@/types/index";
+
+export const navigation: TNavigation = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Work", path: "/work" },
+  { name: "Blog", path: "/blog" },
+  { name: "Contact", path: "/contact" },
+];
---
+++ src/types/index.d.ts
@@ -0,0 +1,9 @@
+export type TNavigation = {
+  name: string;
+  path: string;
+}[];
+
+export type TSocialMediaLinks = {
+  name: string;
+  path: string;
+}[];
