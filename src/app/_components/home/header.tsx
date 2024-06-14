1ecc69f92b33e93ceb817b47dcbe2ebaac8fa6de
Immanuel Ehsan <foxeian26@gmail.com>
2024-06-14 04:26:09 +0300

    change width size of header tag

--- src/app/_components/home/header.tsx
+++ src/app/_components/home/header.tsx
@@ -7,7 +7,7 @@ const bebasNeue = Bebas_Neue({
 
 export default function Header() {
   return (
-    <header className="flex h-screen w-screen flex-col items-start justify-end bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-indigo-500 via-indigo-300 to-indigo-100 p-6 sm:p-14">
+    <header className="flex h-screen w-full flex-col items-start justify-end bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-indigo-500 via-indigo-300 to-indigo-100 p-6 sm:p-14">
       <h1
         className={`${bebasNeue.className} text-8xl tracking-widest text-black md:text-[10rem] lg:text-[15rem]`}
       >
