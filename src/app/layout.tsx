164c9ff68438415b74834a83b23dd6af1817d997
Immanuel Ehsan <foxeian26@gmail.com>
2024-10-07 02:28:01 +0300

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`bg-black font-['HelveticaNeue']`}>
+    <html lang="en" className="bg-black font-['HelveticaNeue']">
       <body>
         <TRPCReactProvider>
           <Navbar />
